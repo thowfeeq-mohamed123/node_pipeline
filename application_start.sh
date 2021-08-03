50a2b2cb767f664922246765f475c3446520c005
mohamed dowbi <mohameddowbi6@gmail.com>
2021-08-03 17:28:56 +0530

    changes

--- application_start.sh
+++ application_start.sh
@@ -13,4 +13,4 @@ export NVM_DIR="${HOME}/.nvm"
 npm install
 
 #start our node app in background
-node server.js > server.out.log 2> server.err.log < /dev/null &
+node app.js > app.out.log 2> app.err.log < /dev/null &
